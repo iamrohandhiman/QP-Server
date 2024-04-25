f3ce03bfff23c913e6d906f269593aaeb46d3f6a
iamrohandhiman <iamrohandhiman@gmail.com>
2024-04-26 00:27:07 +0530

    introduced express validation for metaInfo Route

--- middleware/validateMetaInfo.js
+++ middleware/validateMetaInfo.js
@@ -1,32 +1,10 @@
-export const validateMetaInfo = (req, res, next) => {
-  const {
-    SubjectName,
-    Weights,
-    chapterWiseTheoryRatio,
-    ISE1Chapters,
-    ISE2Chapters,
-    ESEChapters,
-    ISE1TheoryRatio,
-    ISE2TheoryRatio,
-    ESETheoryRatio,
-  } = req.body;
+import { body, validationResult } from 'express-validator';
 
-  const missingFields = [];
-
-
-  if (!SubjectName) missingFields.push("SubjectName");
-  if (!Weights) missingFields.push("Weights");
-  if (!chapterWiseTheoryRatio) missingFields.push("chapterWiseTheoryRatio");
-  if (!ISE1Chapters) missingFields.push("ISE1Chapters");
-  if (!ISE2Chapters) missingFields.push("ISE2Chapters");
-  if (!ESEChapters) missingFields.push("ESEChapters");
-  if (!ISE1TheoryRatio) missingFields.push("ISE1TheoryRatio");
-  if (!ISE2TheoryRatio) missingFields.push("ISE2TheoryRatio");
-  if (!ESETheoryRatio) missingFields.push("ESETheoryRatio");
-
-  if (missingFields.length > 0) {
-    return res.status(400).send(`Missing required fields: ${missingFields.join(", ")}`);
-  }
-
-  next(); 
-};
+export const validateMetaInfo = [
+  body('SubjectName')
+    .notEmpty()
+    .withMessage('SubjectName must not be empty')
+    .isString()
+    .withMessage('SubjectName must be a string'),
+  
+];
--- routes/metaInfo.js
+++ routes/metaInfo.js
@@ -1,21 +1,31 @@
-import express from 'express';
-import { validateMetaInfo } from '../middleware/validateMetaInfo.js';
-
+import express from "express";
+import { validateMetaInfo } from "../middleware/validateMetaInfo.js";
+import {validationResult} from "express-validator"
 const router = express.Router();
+router.post(
+  '/MetaInfo',
+  validateMetaInfo, 
+  (req, res) => {
+    const errors = validationResult(req); 
 
-router.post('/MetaInfo', validateMetaInfo, (req, res) => {
-  const {
-    SubjectName,
-    Weights,
-    chapterWiseTheoryRatio,
-    ISE1Chapters,
-    ISE2Chapters,
-    ESEChapters,
-    ISE1TheoryRatio,
-    ISE2TheoryRatio,
-    ESETheoryRatio,
-  } = req.body;
-  res.sendStatus(200)
-});
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() }); 
+    }
+
+    const {
+      SubjectName,
+      Weights,
+      chapterWiseTheoryRatio,
+      ISE1Chapters,
+      ISE2Chapters,
+      ESEChapters,
+      ISE1TheoryRatio,
+      ISE2TheoryRatio,
+      ESETheoryRatio,
+    } = req.body;
+
+    res.sendStatus(200);
+  }
+);
 
 export default router;
